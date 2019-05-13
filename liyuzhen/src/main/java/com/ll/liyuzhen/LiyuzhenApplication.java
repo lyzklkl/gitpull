0c5924f6ab22b02130cef38a4ad75d0919e54844
llyxz93 <8679138@qq.com>
2019-05-13 19:30:19 +0800

    李育真

---
+++ liyuzhen/src/main/java/com/ll/liyuzhen/LiyuzhenApplication.java
@@ -0,0 +1,13 @@
+package com.ll.liyuzhen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiyuzhenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiyuzhenApplication.class, args);
+    }
+
+}
